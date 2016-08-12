f71019fde2122d52c28a0f4bd966c05709c9156b
User <User@WIN-7Q9SPJR04GV>
2016-08-12 10:02:05 +0300

    Add project files.

---
+++ WebApplication7/Articles.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Configuration;
+
+namespace WebApplication7
+{
+    public partial class Articles : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var connectionString = WebConfigurationManager.ConnectionStrings["FlexBricksTestConnectionString"].ToString();
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = "insert into Articles(articleTitle,ArticleText) values('"+this.TextBox1.Text+"','"+this.TextBox2.Text+"')";
+                command.ExecuteNonQuery();
+            }
+            this.Response.Redirect("Articles.aspx");
+        }
+    }
+}
---
+++ WebApplication7/Models/ArticleModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication7.Models
+{
+    public class ArticleModel
+    {
+        public int ArticleID { get; set; }
+        public string ArticleTitle { get; set; }
+        public string ArticleText { get; set; }
+    }
+}
